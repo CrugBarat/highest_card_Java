17064c8bdbdb783a633706bc26261a5f9720c8f8
CrugBarat <craigtbarratt@gmail.com>
2020-05-28 15:09:54 +0100

    adds addCards method
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -8,4 +8,13 @@ public class Deck {
         this.deck = new ArrayList<Card>();
     }
 
+    public void addCards() {
+        for (int i = 0; i < 13; i++) {
+            CardValue value = CardValue.values()[i];
+            for (int j = 0; j < 4; j++) {
+                Card card = new Card(CardSuit.values()[j], value);
+                this.deck.add(card);
+            }
+        }
+    }
 }
