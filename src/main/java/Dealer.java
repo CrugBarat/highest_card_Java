629b898b3d3b192f5ebb4ef85b29afcd2907f800
CrugBarat <craigtbarratt@gmail.com>
2020-05-28 15:57:29 +0100

    adds Dealer class and with getter and constructor
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,12 @@
+public class Dealer {
+
+    private String name;
+
+    public Dealer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
