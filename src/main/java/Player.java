85261491af1b6c561f4e6a19fe24e23fabd52cb9
CrugBarat <craigtbarratt@gmail.com>
2020-05-28 15:52:46 +0100

    adds Player Class and constructor
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    private String name;
+    private ArrayList<Card> cards;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
