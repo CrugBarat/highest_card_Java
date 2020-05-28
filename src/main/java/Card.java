747f9d0e026ac498cba82bf43ee08a175097abbc
CrugBarat <craigtbarratt@gmail.com>
2020-05-28 14:04:24 +0100

    initial commit
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/Card.java
@@ -0,0 +1,14 @@
+public class Card {
+
+    private CardSuit suit;
+    private CardValue value;
+
+    public Card (CardSuit suit, CardValue value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public CardSuit getSuit() {
+        return this.suit;
+    }
+}
---
+++ src/test/java/CardTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest {
+
+    private Card card;
+
+    @Before
+    public void before() {
+        card = new Card(CardSuit.CLUBS, CardValue.ACE);
+    }
+
+    @Test
+    public void canGetSuit() {
+        assertEquals(CardSuit.CLUBS, card.getSuit());
+    }
+
+
+}
